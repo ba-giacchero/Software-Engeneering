b6354d6f6c7b05d30aecbfa09a2b945930e72c57
ba-giacchero <ba.giacchero@gmail.com>
2025-10-03 13:18:22 +0200

    test github

--- version 1/Bitpacking.java
+++ version 1/Bitpacking.java
@@ -1,8 +1,8 @@
 public class BitPacking{
     public Array<Integer> table
 
-    public BitPacking(Array<Integer> table){
-        self.table = table; 
+    public BitPacking(Array<Integer> tabl){
+        self.table = tabl; 
     }
 
     public Array<Integer> compress(Array<Integer> table){
