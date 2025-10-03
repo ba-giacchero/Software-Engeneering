d4ac210d9047aea625323a4d9c6f81138234f93a
ba-giacchero <ba.giacchero@gmail.com>
2025-10-03 13:03:47 +0200

    init

---
+++ version 1/Bitpacking.java
@@ -0,0 +1,19 @@
+public class BitPacking{
+    public Array<Integer> table
+
+    public BitPacking(Array<Integer> table){
+        self.table = table; 
+    }
+
+    public Array<Integer> compress(Array<Integer> table){
+
+    }
+    
+    public void compress(Array<Integer> table){
+        
+    }
+
+    public int get(int i){
+        
+    }
+}
